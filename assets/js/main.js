2f02503b81b21d7ad40632b40eacc3bc886d6894
Sajid Nazeer <codercryptical@gmail.com>
2022-08-02 22:46:36 -0700

    Olivia

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,6 +137,13 @@ function navHighlighter() {
 }
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll__header");
+  else nav.classList.remove("scroll__header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
 
